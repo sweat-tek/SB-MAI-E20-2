c2df890f68a6398349c98918adc49042b3799016
kvinkel <43136038+kvinkel@users.noreply.github.com>
2020-11-14 16:50:42 +0100

    Add test for setBounds()

--- JHotDraw/src/test/java/org/jhotdraw/samples/svg/figures/SVGImageFigureTest.java
+++ JHotDraw/src/test/java/org/jhotdraw/samples/svg/figures/SVGImageFigureTest.java
@@ -4,12 +4,21 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.awt.geom.Point2D;
+
 import static org.junit.Assert.*;
 
 public class SVGImageFigureTest {
 
+    private SVGImageFigure svgImageFigure;
+    private Point2D.Double point2DAnchor;
+    private Point2D.Double point2DLead;
+
     @Before
     public void setUp() throws Exception {
+        svgImageFigure = new SVGImageFigure(50, 50, 50, 50);
+        point2DAnchor = new Point2D.Double(5.0, 10.0);
+        point2DLead = new Point2D.Double(30.0, 20.0);
     }
 
     @After
@@ -17,6 +26,11 @@ public class SVGImageFigureTest {
     }
 
     @Test
-    public void draw() {
+    public void setBounds() {
+        svgImageFigure.setBounds(point2DAnchor, point2DLead);
+        assertEquals(5.0, svgImageFigure.getX(), 0.0);
+        assertEquals(10.0, svgImageFigure.getY(), 0.0);
+        assertEquals(30.0 - 5.0, svgImageFigure.getWidth(), 0.0);
+        assertEquals(20.0 - 10.0, svgImageFigure.getHeight(), 0.0);
     }
 }
