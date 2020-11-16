05daad90945303a50a0ba5ab6429289d6a61fdb1
AleksanderGD <agdbusiness@hotmail.com>
2020-11-16 19:03:50 +0100

    cleaned code, refactored namings

--- JHotDraw/src/test/java/org/jhotdraw/samples/svg/figures/LinkHandleTest.java
+++ JHotDraw/src/test/java/org/jhotdraw/samples/svg/figures/LinkHandleTest.java
@@ -6,29 +6,44 @@
 package org.jhotdraw.samples.svg.figures;
 
 import java.awt.Rectangle;
-import java.awt.geom.Point2D;
-import java.awt.geom.Rectangle2D;
+import org.jhotdraw.draw.BezierFigure;
+import org.jhotdraw.draw.DefaultDrawingEditor;
 import org.jhotdraw.draw.DefaultDrawingView;
-import org.jhotdraw.draw.Figure;
-import org.jhotdraw.draw.LineFigure;
+import org.jhotdraw.draw.DrawingEditor;
+import org.jhotdraw.draw.DrawingView;
+import org.jhotdraw.geom.BezierPath;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import static org.mockito.Mockito.spy;
 import static org.junit.Assert.*;
 
-/**
- *
- * @author ALEKSTUD
- */
 public class LinkHandleTest {
- 
-    private static Figure owner;
-    private static LinkHandle ah;
-    private static DefaultDrawingView view; // view = DefaultDrawingView
-    private static Rectangle2D.Double b; // x = 125.0, y = 78.0, width= 192.0, height 148.0
-    private static Point2D.Double p;
+
+    private BezierFigure bezierFigure;
+    @Mock
+    private LinkHandle linkHandle;
+    private LinkHandle linkHandleTest;
+
+    public void setUpMock() {
+        bezierFigure = new BezierFigure();
+        bezierFigure.addNode(new BezierPath.Node(10, 10));
+        bezierFigure.addNode(new BezierPath.Node(20, 20));
+        linkHandleTest = spy(new LinkHandle(bezierFigure));
+        
+        DrawingView defaultDrawingView = new DefaultDrawingView();
+        DrawingEditor defaultDrawingEditor = new DefaultDrawingEditor();
+
+        linkHandle = Mockito.mock(LinkHandle.class);
+        Mockito.doReturn(linkHandle).when(linkHandleTest).makeLinkHandle(bezierFigure);
+
+        defaultDrawingEditor.setActiveView(defaultDrawingView);
+        linkHandleTest.setView(defaultDrawingView);
+    }
 
     public LinkHandleTest() {
     }
@@ -44,36 +59,22 @@ public class LinkHandleTest {
 
     @Before
     public void setUp() {
-        owner = new LineFigure();
-        ah = new LinkHandle(owner);
-        b = new Rectangle2D.Double(125.0, 78.0, 192.0, 148.0);
-        view = new DefaultDrawingView();
-        p = new Point2D.Double(b.x + b.width, b.y + b.height);
+        setUpMock();
     }
 
     @After
     public void tearDown() {
-        owner = null;
-        ah = null;
-        b = null;
-        view = null;
-        p = null;
     }
 
     @Test
     public void testBasicGetBounds() {
         System.out.println("testBasicGetBounds()");
-        Rectangle actualRectangle = new Rectangle(view.drawingToView(p));
-        int h = 7;
-        actualRectangle.x -= h * 4; 
-        actualRectangle.y -= h;
-        actualRectangle.width = h * 2;
-        actualRectangle.height = h;
-
-        Rectangle expectedRectangle = new Rectangle(289, 219, 14, 7);
+        Rectangle actualRectangle = linkHandleTest.basicGetBounds();
+       
+        Rectangle expectedRectangle = new Rectangle(16, 19, 2, 1);
         System.out.println("expected:" + expectedRectangle.toString());
         System.out.println("actual:" + actualRectangle.toString());
         assertEquals(expectedRectangle, actualRectangle);
-       // Expected Rectangle [289, 219, 14, 7]
+        // Expected Rectangle [16, 19, 2, 1];
     }
 }
