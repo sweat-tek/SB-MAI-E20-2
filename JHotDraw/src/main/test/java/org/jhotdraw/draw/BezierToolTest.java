385a8fc487c996f8fdc53adb2edb130be86dda6f
Sebastian <sebastian@moldt.dk>
2020-11-18 21:41:32 +0100

    Update BezierToolTest.java

--- JHotDraw/src/main/test/java/org/jhotdraw/draw/BezierToolTest.java
+++ JHotDraw/src/main/test/java/org/jhotdraw/draw/BezierToolTest.java
@@ -11,22 +11,22 @@ public class BezierToolTest {
 
     private DefaultDrawing drawing;
     private BezierFigure bezierFigure;
-    private BezierTool bezierTool;
 
     @Before
     public void setUp() throws Exception {
         System.out.println("Setting up.");
         drawing = new DefaultDrawing();
-        bezierTool = new BezierTool(bezierFigure);
+        bezierFigure = new BezierFigure();
+        drawing.add(bezierFigure);
         bezierFigure = new BezierFigure();
         drawing.add(bezierFigure);
         assertTrue(drawing.getChildren().contains(bezierFigure));
+        assertEquals(2, drawing.getChildCount());
     }
 
     @After
     public void tearDown() throws Exception {
         System.out.println("Tearing down.");
-        bezierTool = null;
         bezierFigure = null;
         drawing = null;
     }
@@ -34,16 +34,28 @@ public class BezierToolTest {
     @Test
     public void undo() {
         System.out.println("Testing undo event.");
-        drawing.remove(bezierFigure);
-        assertFalse(drawing.getChildren().contains(bezierFigure));
+        int latestDrawingIndex = drawing.getChildren().lastIndexOf(bezierFigure);
+        BezierFigure latestFigure = (BezierFigure) drawing.getChild(latestDrawingIndex);
+        drawing.remove(latestFigure);
+        assertFalse(drawing.getChildren().contains(latestFigure));
+        assertEquals(1, drawing.getChildCount());
+
+        BezierFigure remainingFigure = (BezierFigure) drawing.getChild(latestDrawingIndex-1);
+        drawing.remove(remainingFigure);
+        assertFalse(drawing.getChildren().contains(remainingFigure));
+        assertEquals(0, drawing.getChildCount());
     }
 
     @Test
     public void redo() {
         System.out.println("Testing redo event.");
-        drawing.remove(bezierFigure);
-        assertFalse(drawing.getChildren().contains(bezierFigure));
-        drawing.add(bezierFigure);
-        assertTrue(drawing.getChildren().contains(bezierFigure));
+        int latestDrawing = drawing.getChildren().lastIndexOf(bezierFigure);
+        BezierFigure latestFigure = (BezierFigure) drawing.getChild(latestDrawing);
+        drawing.remove(latestFigure);
+        assertFalse(drawing.getChildren().contains(latestFigure));
+        assertEquals(1, drawing.getChildCount());
+        drawing.add(latestFigure);
+        assertTrue(drawing.getChildren().contains(latestFigure));
+        assertEquals(2, drawing.getChildCount());
     }
 }
